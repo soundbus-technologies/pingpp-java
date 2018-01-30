51f7945f4c8d9b548b2da3c3813cebdf65a45b21
wangoo <wangoo@soundbus.cn>
2018-01-30 22:24:56 +0800

    single app id mode

--- src/main/java/com/pingplusplus/AppContextHolder.java
+++ src/main/java/com/pingplusplus/AppContextHolder.java
@@ -1,15 +1,29 @@
 package com.pingplusplus;
 
 public abstract class AppContextHolder {
-
     private static ThreadLocal<String> appIdLocal = new ThreadLocal<String>();
     private static ThreadLocal<PingLogger> PingLogger = new ThreadLocal<PingLogger>();
+    private static boolean singleAppIdMode = false;
+    private static String singleAppId = null;
+
+    public static void enableSingleAppIdMode(String appId) {
+        singleAppIdMode = true;
+        singleAppId = appId;
+    }
+
+    public static void disableSingleAppIdMode() {
+        singleAppIdMode = false;
+        singleAppId = null;
+    }
 
     public static void setAppId(String appId) {
         appIdLocal.set(appId);
     }
 
     public static String getAppId() {
+        if (singleAppIdMode) {
+            return singleAppId;
+        }
         return appIdLocal.get();
     }
 
